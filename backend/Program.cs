6237a4cac87d79703e964e8025317ae6c998dcfd
Mexoor <73921858+Mexoor@users.noreply.github.com>
2025-09-10 08:10:07 +0200

    LandingPage UI

--- backend/Program.cs
+++ backend/Program.cs
@@ -15,6 +15,17 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") // Replace with your frontend's URL/port
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
 // Add services to the container.
 builder.Services.AddScoped<IResourceRepository, ResourceRepository>();
 builder.Services.AddScoped<IResourceService, ResourceService>();
@@ -91,7 +102,7 @@ using (var scope = app.Services.CreateScope())
     await DbSeeder.SeedRolesAndUsersAsync(roleManager, userManager);
 }
 
-
+app.UseCors("AllowFrontend");
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
