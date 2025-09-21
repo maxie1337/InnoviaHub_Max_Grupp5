03e6da40e6510967b872e035caab10a26a4accd2
HeidiDragomir <heididragomir@gmail.com>
2025-09-21 09:57:29 +0200

    Add frontend link to the cors

--- backend/Program.cs
+++ backend/Program.cs
@@ -81,7 +81,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("FrontendPolicy", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins(
+            "http://localhost:5173",    // development
+            "https://innovia-hub.netlify.app"   // production
+            )
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
