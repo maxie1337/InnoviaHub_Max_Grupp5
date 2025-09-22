27c53d099bc53004d778782749ca29df2b684fa0
adammattsson1 <adammattsson92@gmail.com>
2025-09-22 19:13:58 +0200

    mybooking ändringar

--- frontend/src/components/MyBooking/MyBookingCard.tsx
+++ frontend/src/components/MyBooking/MyBookingCard.tsx
@@ -8,47 +8,26 @@ interface ResourceCardProps {
 }
 
 export default function MyBookingCard({ booking, onCancel }: ResourceCardProps) {
-  const [startDateString, setStartString] = useState("");
-  const [endDateString, setEndString] = useState("");
+  const [bookingString, setBookingString] = useState("");
 
-  function createDateStrings() {
-    let startDate = new Date(booking.bookingDate).toLocaleString();
-    let endDate = new Date(booking.endDate).toLocaleString();
-
-    startDate = startDate.substring(0, startDate.length - 3);
-    endDate = endDate.substring(0, endDate.length - 3);
-
-    setStartString(startDate);
-    setEndString(endDate);
-  }
-
-  useEffect(() => {
-    createDateStrings();
-  }, []);
-
-  //
-  let badgeText = "My Booking";
-  let badgeStyle = "bg-blue-100 text-blue-800";
-
-  if (!booking.isActive) {
-    badgeText = "Cancelled";
-    badgeStyle = "bg-gray-100 text-gray-800";
-  }
+  useEffect(() => 
+  {
+    var date = new Date(booking.bookingDate);
+    var string = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    setBookingString(string);
+  }, [])
 
   return (
-    <div className="rounded-lg p-4 flex flex-col justify-between bg-white border border-gray-200 shadow-sm">
-
-      <div className="flex items-center justify-between mb-4">
-        <h3 className="text-lg font-semibold">{booking.resource.name}</h3>
-        <span
-          className={`inline-flex px-3 py-1 text-xs font-semibold rounded-full ${badgeStyle}`}
-        >
-          {badgeText}
-        </span>
+    <div className="rounded-lg p-4 flex flex-col">
+      <div>
+        <h3 className="text-lg font-semibold text-center">{booking.resource.name}</h3>
       </div>
 
       <p className="text-sm mb-4">
-        {startDateString} – {endDateString}
+        {bookingString} 
+      </p>
+      <p className="text-sm mb-4">
+        {`${new Date(booking.bookingDate).getHours()}:00-${new Date(booking.endDate).getHours()}:00`}
       </p>
 
       {booking.isActive && (
--- frontend/src/pages/MyBookings.tsx
+++ frontend/src/pages/MyBookings.tsx
@@ -12,6 +12,7 @@ const MyBookings: React.FC = () => {
     
     //State for bookings and loading
     const [myBookings, setMyBookings] = useState<Booking[]>([]);
+    const [sortedBookings, setSortedBookings] = useState<Booking[]>([]);
     const [loading, setLoading] = useState(true);
 
     //SignalR URL
@@ -82,6 +83,27 @@ const MyBookings: React.FC = () => {
         };
     }, [token, hubUrl]);
 
+    useEffect(() => 
+    {
+        var bookings = myBookings;
+        var sorted = bookings;
+
+        for (var i = 0; i < sorted.length; i++) 
+        {
+            for (var j = 0; j < (sorted.length - i - 1); j++) 
+            {
+                if (Date.parse(sorted[j].bookingDate) > Date.parse(sorted[j + 1].bookingDate)) 
+                {
+                    var temp = sorted[j]
+                    sorted[j] = sorted[j + 1]
+                    sorted[j + 1] = temp
+                }
+            }
+        }
+
+        setSortedBookings(sorted);
+    }, [myBookings])
+
     const handleCancel = async (bookingId: number) => 
     {
         if (!token) return;
@@ -112,10 +134,12 @@ const MyBookings: React.FC = () => {
             <br />
     
             <div className="grid gap-6"
-            style={{ gridTemplateColumns: "repeat(auto-fit, minmax(400px, 400px))", justifyContent: "center", maxWidth: "1200px", margin: "0 auto",}}>
-                {myBookings.map((b) => 
+            style={{ gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))", justifyContent: "center", maxWidth: "1200px", margin: "0 auto",}}>
+                {sortedBookings.map((b) => 
                 (
-                    <MyBookingCard booking={b} onCancel={handleCancel}/>
+                    <div className="bg-white rounded-xl border border-gray-200 p-6 shadow-sm">
+                        <MyBookingCard booking={b} onCancel={handleCancel}/>
+                    </div>
                 ))}
             </div>
             {myBookings.length == 0 ? <p className="text-center">You have no active bookings.</p> : null}
